fdb56912fb1cb78c7040a3d798cdb58a931a5051
Nicholas Costello <nicholasjcostello@gmail.com>
2020-05-04 07:35:47 -0400

    Added Db Initial migration

---
+++ EmployeeManagement.Api/Migrations/20200503093140_initialCreate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emloyees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emloyees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "Stark" },
+                    { 2, "Lannister" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Emloyees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "DepartmentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1989, 3, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "JSnow@gmail.com", "John", 0, "Snow", "" },
+                    { 2, new DateTime(1980, 3, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "PVPMeBro@gmail.com", "Jamie", 0, "Lannister", "" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Emloyees");
+        }
+    }
+}
---
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EmployeeManagement.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Emloyees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 1, DepartmentName = "Stark"}
+                );
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 2, DepartmentName = "Lannister" }
+                );
+
+            //Seed Employee Table
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    EmployeeId = 1,
+                    FirstName = "John",
+                    LastName = "Snow",
+                    Email = "JSnow@gmail.com",
+                    DateOfBirth = new DateTime(1989,3,10),
+                    Gender = Gender.Male,
+                    DepartmentId = 0,
+                    PhotoPath = ""
+                 }
+             );
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    EmployeeId = 2,
+                    FirstName = "Jamie",
+                    LastName = "Lannister",
+                    Email = "PVPMeBro@gmail.com",
+                    DateOfBirth = new DateTime(1980, 3, 10),
+                    Gender = Gender.Male,
+                    DepartmentId = 1,
+                    PhotoPath = ""
+                }
+             );
+
+        }
+    }
+}
